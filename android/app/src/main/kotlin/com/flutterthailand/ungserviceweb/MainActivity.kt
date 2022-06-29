410082d6339f2498301893fbb7bb3e0d8053d39d
ung <test@abc.com>
2022-06-29 14:51:36 +0700

    Design UI authen success

---
+++ android/app/src/main/kotlin/com/flutterthailand/ungserviceweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterthailand.ungserviceweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utility/my_dialog.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:ungserviceweb/utility/my_constant.dart';
+import 'package:ungserviceweb/widgets/show_image.dart';
+import 'package:ungserviceweb/widgets/show_text.dart';
+import 'package:ungserviceweb/widgets/show_text_button.dart';
+
+class MyDialog {
+  final BuildContext context;
+  MyDialog({
+    required this.context,
+  });
+
+  Future<void> normalDialog(
+      {required String title, required String subTitle}) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: ListTile(
+          leading: const SizedBox(
+            width: 72,
+            child: ShowImage(),
+          ),
+          title: ShowText(
+            text: title,
+            textStyle: MyConstant().h2Style(),
+          ),
+          subtitle: ShowText(text: subTitle),
+        ),
+        actions: [
+          ShowTextButton(
+            label: 'OK',
+            pressFunc: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
